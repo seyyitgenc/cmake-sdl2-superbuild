c8d09f5e6a01f5ba52dc9d90518e7cbff3b48e08
Seyyit Genç <mahluk053@gmail.com>
2022-11-25 17:42:28 +0300

    main

---
+++ build/SDL/CMakeFiles/SDL2-static.dir/src/audio/dummy/SDL_dummyaudio.c.obj.d
@@ -0,0 +1,66 @@
+SDL/CMakeFiles/SDL2-static.dir/src/audio/dummy/SDL_dummyaudio.c.obj: \
+ C:/projects/helloworld/SDL/src/audio/dummy/SDL_dummyaudio.c \
+ C:/projects/helloworld/SDL/src/SDL_internal.h \
+ C:/projects/helloworld/SDL/src/dynapi/SDL_dynapi.h \
+ C:/projects/helloworld/SDL/src/dynapi/SDL_dynapi_overrides.h \
+ C:/projects/helloworld/build/SDL/include-config-debug/SDL_config.h \
+ C:/projects/helloworld/build/SDL/include/SDL_platform.h \
+ C:/projects/helloworld/build/SDL/include/begin_code.h \
+ C:/projects/helloworld/build/SDL/include/close_code.h \
+ C:/projects/helloworld/build/SDL/include/SDL_assert.h \
+ C:/projects/helloworld/build/SDL/include/SDL_stdinc.h \
+ C:/msys64/mingw64/include/sys/types.h \
+ C:/msys64/mingw64/include/crtdefs.h C:/msys64/mingw64/include/corecrt.h \
+ C:/msys64/mingw64/include/_mingw.h \
+ C:/msys64/mingw64/include/_mingw_mac.h \
+ C:/msys64/mingw64/include/_mingw_secapi.h \
+ C:/msys64/mingw64/include/vadefs.h \
+ C:/msys64/mingw64/include/sdks/_mingw_ddk.h \
+ C:/msys64/mingw64/include/_mingw_off_t.h \
+ C:/msys64/mingw64/include/stdio.h \
+ C:/msys64/mingw64/include/corecrt_stdio_config.h \
+ C:/msys64/mingw64/include/swprintf.inl \
+ C:/msys64/mingw64/include/sec_api/stdio_s.h \
+ C:/msys64/mingw64/include/stdlib.h \
+ C:/msys64/mingw64/include/corecrt_wstdlib.h \
+ C:/msys64/mingw64/include/limits.h \
+ C:/msys64/mingw64/include/sec_api/stdlib_s.h \
+ C:/msys64/mingw64/include/malloc.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/12.2.0/include/mm_malloc.h \
+ C:/msys64/mingw64/include/errno.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/12.2.0/include/stddef.h \
+ C:/msys64/mingw64/include/stddef.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/12.2.0/include/stdarg.h \
+ C:/msys64/mingw64/include/stdarg.h \
+ C:/msys64/mingw64/include/_mingw_stdarg.h \
+ C:/msys64/mingw64/include/string.h \
+ C:/msys64/mingw64/include/sec_api/string_s.h \
+ C:/msys64/mingw64/include/strings.h C:/msys64/mingw64/include/wchar.h \
+ C:/msys64/mingw64/include/_mingw_stat64.h \
+ C:/msys64/mingw64/include/sec_api/wchar_s.h \
+ C:/msys64/mingw64/include/inttypes.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/12.2.0/include/stdint.h \
+ C:/msys64/mingw64/include/stdint.h C:/msys64/mingw64/include/ctype.h \
+ C:/msys64/mingw64/include/math.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/12.2.0/include/float.h \
+ C:/msys64/mingw64/include/float.h \
+ C:/projects/helloworld/build/SDL/include/SDL_log.h \
+ C:/projects/helloworld/build/SDL/include/SDL_timer.h \
+ C:/projects/helloworld/build/SDL/include/SDL_error.h \
+ C:/projects/helloworld/build/SDL/include/SDL_audio.h \
+ C:/projects/helloworld/build/SDL/include/SDL_endian.h \
+ C:/projects/helloworld/build/SDL/include/SDL_mutex.h \
+ C:/projects/helloworld/build/SDL/include/SDL_thread.h \
+ C:/projects/helloworld/build/SDL/include/SDL_atomic.h \
+ C:/projects/helloworld/build/SDL/include/SDL_platform.h \
+ C:/msys64/mingw64/include/process.h \
+ C:/msys64/mingw64/include/corecrt_startup.h \
+ C:/projects/helloworld/build/SDL/include/SDL_rwops.h \
+ C:/projects/helloworld/SDL/src/audio/SDL_audio_c.h \
+ C:/projects/helloworld/SDL/src/SDL_internal.h \
+ C:/projects/helloworld/SDL/src/audio/dummy/SDL_dummyaudio.h \
+ C:/projects/helloworld/SDL/src/audio/SDL_sysaudio.h \
+ C:/projects/helloworld/build/SDL/include/SDL_mutex.h \
+ C:/projects/helloworld/build/SDL/include/SDL_thread.h \
+ C:/projects/helloworld/SDL/src/SDL_dataqueue.h \
+ C:/projects/helloworld/SDL/src/audio/SDL_audio_c.h
---
+++ build/SDL/CMakeFiles/SDL2_test.dir/cmake_clean.cmake
@@ -0,0 +1,41 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_assert.c.obj"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_assert.c.obj.d"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_common.c.obj"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_common.c.obj.d"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_compare.c.obj"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_compare.c.obj.d"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_crc32.c.obj"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_crc32.c.obj.d"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_font.c.obj"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_font.c.obj.d"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_fuzzer.c.obj"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_fuzzer.c.obj.d"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_harness.c.obj"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_harness.c.obj.d"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_imageBlit.c.obj"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_imageBlit.c.obj.d"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_imageBlitBlend.c.obj"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_imageBlitBlend.c.obj.d"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_imageFace.c.obj"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_imageFace.c.obj.d"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_imagePrimitives.c.obj"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_imagePrimitives.c.obj.d"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_imagePrimitivesBlend.c.obj"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_imagePrimitivesBlend.c.obj.d"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_log.c.obj"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_log.c.obj.d"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_md5.c.obj"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_md5.c.obj.d"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_memory.c.obj"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_memory.c.obj.d"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_random.c.obj"
+  "CMakeFiles/SDL2_test.dir/src/test/SDL_test_random.c.obj.d"
+  "libSDL2_test.a"
+  "libSDL2_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SDL2_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
