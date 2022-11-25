28240c25b8e8c1406b67a61d6526c8200160fcb4
Seyyit Genç <mahluk053@gmail.com>
2022-11-25 17:45:58 +0300

    final

--- build/SDL/include/SDL_revision.h
+++
@@ -1,8 +0,0 @@
-/* #undef SDL_VENDOR_INFO */
-#define SDL_REVISION_NUMBER 0
-
-#ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-release-2.26.0-12-g859cb13ef (" SDL_VENDOR_INFO ")"
-#else
-#define SDL_REVISION "SDL-release-2.26.0-12-g859cb13ef"
-#endif
